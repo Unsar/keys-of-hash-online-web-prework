f0032856c8d5b9c33527102eca3fef6867b796f1
Unsar Malik <unsar.malik24@gmail.com>
2019-04-15 01:52:14 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -9,7 +9,15 @@ def animals
 end 
 
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+    end
+    return array
   end
 end
